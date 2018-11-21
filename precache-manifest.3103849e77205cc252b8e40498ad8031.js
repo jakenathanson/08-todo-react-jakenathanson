11798c692ba97c0b0a98433b35947e34bc9938a4
jake <jake.nathanson@me.com>
2018-11-20 22:44:12 -0500

    Updates

---
+++ precache-manifest.3103849e77205cc252b8e40498ad8031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f422066172f80e73aa9f",
+    "url": "/08-todo-react-jakenathanson/static/js/runtime~main.f4220661.js"
+  },
+  {
+    "revision": "59f9732df4cefa6dac45",
+    "url": "/08-todo-react-jakenathanson/static/js/main.59f9732d.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/08-todo-react-jakenathanson/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "59f9732df4cefa6dac45",
+    "url": "/08-todo-react-jakenathanson/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "f953c33939e57304c4e7ae3859810664",
+    "url": "/08-todo-react-jakenathanson/index.html"
+  }
+];
