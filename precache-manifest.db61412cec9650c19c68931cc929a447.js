09be9bd20072e8732d2773c08645e2123f86517e
jake <jake.nathanson@me.com>
2018-11-20 22:40:46 -0500

    Updates

---
+++ precache-manifest.db61412cec9650c19c68931cc929a447.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba01b305dc6fd28c287",
+    "url": "/08-todo-react/static/js/runtime~main.2ba01b30.js"
+  },
+  {
+    "revision": "59f9732df4cefa6dac45",
+    "url": "/08-todo-react/static/js/main.59f9732d.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/08-todo-react/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "59f9732df4cefa6dac45",
+    "url": "/08-todo-react/static/css/main.1af24ae9.chunk.css"
+  },
+  {
+    "revision": "ebfe40c2630911dbecc5a90fb98d15fb",
+    "url": "/08-todo-react/index.html"
+  }
+];
