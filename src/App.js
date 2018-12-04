512eaee0821e36559398c15912e64a47946134ba
jake <jake.nathanson@me.com>
2018-12-03 18:19:49 -0600

    piedpiper
    piedpiper

--- src/App.js
+++ src/App.js
@@ -21,6 +21,27 @@ this.onChange = this.onChange.bind(this);
       // stuff here
     }
 
+    componentDidMount() {
+
+var self = this;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+      var temp = JSON.parse(this.responseText);
+      self.setState({todos: temp});
+      console.log(self.state.todos);
+
+  }
+};
+xhttp.open("GET", "https://api.kraigh.net/todos", true);
+xhttp.setRequestHeader("x-api-key","c5419f366b3a56b4e7a461d12362410c9a9f83f45e77ae1ce916ebe9b3db0230");
+xhttp.send();
+
+
+
+
+    }
+
 onChange(event) {
   // Set the state to the value of the input
   this.setState({
@@ -28,23 +49,47 @@ onChange(event) {
   });
 }
 
-    addTodo(event) {
+addTodo(event) {
       // Handle new Todo form submit
       //this.state.input.reset();
       //
-
       const newTodoText = this.state.input;
       console.log(event.target);
       console.log(this.state.input);
       this.state.input='';
       event.preventDefault();
+      console.log(newTodoText);
+      var self = this;
+      var xhttp2 = new XMLHttpRequest();
+      var data = {
+text: newTodoText
+}
 
+      xhttp2.onreadystatechange = function() {
 
+// Wait for readyState = 4 & 200 response
+if (this.readyState == 4 && this.status == 200) {
 
-       //const newTodoText = this.state.input;
-       //this.state.input = '';
-      // Ajax Call
-      //this.setState({...this.state.todos, "k"});
+  // parse JSON response
+  var todo = JSON.parse(this.responseText);
+  console.log(todo);
+
+  self.setState({
+     todos: [...self.state.todos, JSON.parse(this.responseText)]
+   })
+
+} else if (this.readyState == 4) {
+
+  // this.status !== 200, error from server
+  console.log(this.responseText);
+
+}
+};
+
+xhttp2.open("POST", "https://api.kraigh.net/todos", true);
+xhttp2.setRequestHeader("Content-type", "application/json");
+xhttp2.setRequestHeader("x-api-key", "c5419f366b3a56b4e7a461d12362410c9a9f83f45e77ae1ce916ebe9b3db0230");
+xhttp2.send(JSON.stringify(data));
     }
 
 
@@ -62,7 +107,12 @@ onChange(event) {
       </div>
   <ul class="list-group list-group-flush" id="todoList">
   <NewTodo addTodo={this.addTodo} onChange={this.onChange} input={this.state.input}/ >
-   <Todo />
+
+   {this.state.todos.map((todo) =>
+     <Todo key={todo.id} id={todo.id} completed={todo.completed}
+       text={todo.text} removeDeletedTodo={this.removeDeletedTodo}/>
+   )}
+
   </ul>
 </div>
     );
--- src/Todo.js
+++ src/Todo.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 //import './App.css';
-
 var styles = {
   float: 'right',
 };
-var styles1 = {
-  float: 'right',
-  marginRight: '4px',
-};
-
 class Todo extends Component {
   render() {
+    var iscomplete = this.props.completed;
+    if(iscomplete==false)
+    {
+      var complete = {};
+      var styles1 = {
+        float: 'right',
+        marginRight: '4px',
+      };
+  } else {
+      complete = {
+        textDecoration: "line-through",
+        color: "#28a745"
+      }
+      var styles1 = {
+        display: "none"
+      };
+
+    }
     return (
-  <li class="list-group-item">
-  Todo
+  <li class="list-group-item" style={complete}>
+  {this.props.text}
   <span><input type="button" style={styles} class="btn btn-outline-danger btn-sm" value="Delete" ></input></span>
   <span><input type="button" style={styles1} class="btn btn-outline-success btn-sm" value="Check"></input></span>
   </li>
